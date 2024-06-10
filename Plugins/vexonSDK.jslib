db721b52c955d394c9cdc66b28711da87cd54a45
Nathaniel <nathaniel_ramirez@icloud.com>
2024-06-09 20:12:27 -0700

    Upload SDK Files

---
+++ Plugins/vexonSDK.jslib
@@ -0,0 +1,5 @@
+mergeInto(LibraryManager.library, {
+  GameReady: function () {
+    VexonSDK.GameReady();
+  },
+});
---
+++ VexonSDK.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace VexonSDK
+{
+    public static class Instance
+    {
+        [DllImport("__Internal")]
+        private static extern void GameReady();
+
+        public static void Ready()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            GameReady();
+#else
+            Debug.Log("VexonSDK: GameReady");
+#endif
+        }
+    }
+}
